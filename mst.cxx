555c522b47fe6895bf9d997a98d418ada947dcd2
Konrad Gądek <kgadek@gmail.com>
2020-10-30 02:53:10 +0100

    Trivial interface

--- mst.cxx
+++ mst.cxx
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <map>
 #include <mstch/mstch.hpp>
 
-int main() {
-  std::string view{"{{#names}}Hi {{name}}!\n{{/names}}"};
-  mstch::map context{
-    {"names", mstch::array{
-      mstch::map{{"name", std::string{"Chris"}}},
-      mstch::map{{"name", std::string{"Mark"}}},
-      mstch::map{{"name", std::string{"Scott"}}},
-    }}
-  };
-  
-  std::cout << mstch::render(view, context) << std::endl;
-  
+int main(int argc, char *argv[]) {
+  std::ifstream tpl_file{argv[1]};
+  std::string tpl{std::istreambuf_iterator<char>(tpl_file), std::istreambuf_iterator<char>()};
+  mstch::map ctx{};
+
+  for(int i=2; i<argc; i+=2) {
+    ctx.emplace(std::make_pair(std::string(argv[i]), std::string(argv[i+1])));
+  }
+
+  std::cout << mstch::render(tpl, ctx);
   return 0;
 }
 
