33b5efab7bbb69e1d282467faa93f6331f169080
Konrad Gądek <kgadek@gmail.com>
2021-02-20 20:37:05 +0100

    Add 'include' lambda to context
    This allows including other files. Beware of infinite loops!
    
        {{#include}}name_of_the_file{{/include}}

--- mst.cxx
+++ mst.cxx
@@ -10,7 +10,13 @@ int main(int argc, char *argv[]) {
     std::ifstream tpl_fstream{ argv[1] };
     std::string tpl{ std::istreambuf_iterator<char>{ tpl_fstream },
                      std::istreambuf_iterator<char>{} };
-    mstch::map ctx{};
+    mstch::map ctx{
+        {"include", mstch::lambda{ [](const std::string &filename) -> mstch::node {
+            std::ifstream file_fstream{ filename };
+            return std::string { std::istreambuf_iterator<char>{ file_fstream },
+                                 std::istreambuf_iterator<char>{} };
+        }}}
+    };
 
     for(int i=2; i+1<argc; i+=2) {
         ctx.emplace(std::make_pair(std::string(argv[i]), std::string(argv[i+1])));
